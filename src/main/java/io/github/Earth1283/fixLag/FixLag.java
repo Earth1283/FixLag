658ae9ca9f1087e174006add64768d3b1a2ac4d5
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2025-11-29 09:32:28 +0800

    bstats

--- src/main/java/io/github/Earth1283/fixLag/FixLag.java
+++ src/main/java/io/github/Earth1283/fixLag/FixLag.java
@@ -11,6 +11,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import io.github.Earth1283.fixLag.Metrics;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -52,6 +54,8 @@ public class FixLag extends JavaPlugin {
         saveDefaultConfig();
         loadConfig();
         loadMessages();
+        int pluginId = 28156;
+        Metrics metrics = new Metrics(this, pluginId); // bstats
 
         // Initialize MiniMessage and serializer
         miniMessage = MiniMessage.miniMessage();
